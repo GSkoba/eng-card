4ac98c7c13fbcf8f46ee84cfdc70389e91aa3463
Stepan.Vakhonin <Stepan.Vakhonin@nexign.com>
2020-04-11 18:23:28 +0300

    dirty hack for reuse container

--- backend/src/test/kotlin/com/lang/card/engcard/config/MongoContainerConfig.kt
+++ backend/src/test/kotlin/com/lang/card/engcard/config/MongoContainerConfig.kt
@@ -4,16 +4,20 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.testcontainers.containers.FixedHostPortGenericContainer
 import org.testcontainers.containers.GenericContainer
-import java.util.*
+import java.lang.Exception
 
 @TestConfiguration
 class MongoContainerConfig() {
 
     @Bean(destroyMethod = "stop")
-    fun mongoContainer(): GenericContainer<Nothing> {
-        val mongoContainer: GenericContainer<Nothing> = FixedHostPortGenericContainer<Nothing>("mongo")
-                .withFixedExposedPort(27018,27017)
-        mongoContainer.start()
-        return mongoContainer
+    fun mongoContainer(): GenericContainer<Nothing>? {
+        return try {
+            val mongoContainer: GenericContainer<Nothing> = FixedHostPortGenericContainer<Nothing>("mongo")
+                    .withFixedExposedPort(27018, 27017)
+            mongoContainer.start()
+            mongoContainer
+        } catch (ex : Exception) {
+            null
+        }
     }
 }
