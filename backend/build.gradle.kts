51a2aa5d1ec88415e717389699e4ac7b58a88b52
Grigoriy Skobelev <gskobelevff@gmail.com>
2020-04-22 19:10:53 +0300

    user auth

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -20,4 +21,5 @@ dependencies {
     testImplementation("org.testcontainers:testcontainers:1.13.0")
     implementation("io.springfox:springfox-swagger-ui:2.9.2")
     implementation("io.springfox:springfox-swagger2:2.9.2")
+
 }
---
+++ backend/src/main/kotlin/com/lang/card/engcard/config/WebSecBasicConfig.kt
@@ -0,0 +1,26 @@
+package com.lang.card.engcard.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class WebSecBasicConfig : WebSecurityConfigurerAdapter(){
+    companion object {
+        const val ADMIN = "ADMIN"
+        const val USER = "USER"
+    }
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/cards/**").hasRole(ADMIN)
+                .and().formLogin()
+                .and().httpBasic()
+    }
+
+}
